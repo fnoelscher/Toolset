ad934272788a439d73e4a71a0dcfab44ae877087
= <=>
2013-07-04 19:29:09 +0200

    re-arranged testing folder - divided c and c++ for easier building added dynamic c-style template class matrix, stl/"more c++" version will follow added tests for matrix_cstyle

---
+++ basics/matrix/matrix_cstyle.hpp
@@ -0,0 +1,175 @@
+/*    
+    Runtime-Allocated 2D-Matrix - very basic - not even using stl
+       -- this is more c than c++ actually
+
+    Copyright (C) 2013  H. F. Noelscher
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+
+#include <stdio.h>     // for printf
+#include <stdlib.h>    // for malloc / free
+#include <string.h>    // for memset
+
+template <class T>
+class Matrix {
+
+public:
+  
+  Matrix(unsigned int cols,
+	 unsigned int rows)
+  {
+    size_t tsize = sizeof(T);
+
+    // allocate one chunk of data -> better memory locality
+    this->data = (T*) malloc( rows * cols * tsize);
+
+    // allocate array of pointers, the actual matrix
+    this->array = (T**) malloc( rows * sizeof(*array));
+
+    if(! this->data)
+      {
+	printf("An error occured allocating memory\n");
+      }
+    if(! this->array)
+      {
+	printf("An error occured allocating memory (2)\n");
+      }
+
+    // zero memory
+    memset(this->data, 0, (rows * cols * tsize));    
+
+    // create internal array of pointers
+    for(unsigned int i = 0; i < rows; i++)
+      {
+	this->array[i] = &data[i * cols];
+      }
+
+    this->sizey = rows;
+    this->sizex = cols;
+  }
+
+  ~Matrix()
+  {
+    // frees everything - valgrind checked
+    free(this->array);
+    free(this->data);
+  }
+
+  /* --------
+    OPERATORS
+     --------
+  */
+
+  T* operator [](unsigned int index)
+  {
+    return this->array[index];
+  }
+
+  // provides matrix multiplication or dot-product, if sizes are not equal
+  // not yet implemented
+  Matrix& operator *(Matrix &other)
+  {
+    unsigned int rsizex = other.getSizeX();
+    unsigned int rsizey = other.getSizeY();
+
+    if(this->sizex != rsizex ||
+       this->sizey != rsizey)
+      return this->dotProduct(other);
+    else
+      return this->mult(other);
+  }
+
+
+  /* ---------
+    OPERATIONS
+     ---------
+  */
+
+  Matrix& mult(Matrix &other)
+  {
+    // not yet implemented
+    printf("Method 'mult' not yet implemented");
+    return other; 
+  }
+
+  Matrix& dotProduct(Matrix &other)
+  {
+    // not yet implemented
+    printf("Method 'dotProduct' not yet implemented");
+    return other;
+  }
+
+  // in place-multiplicaton
+  void multInPlace(Matrix &other)
+  {
+    if(! hasSameDimensions(other))
+      return;
+    
+    for(unsigned int i = 0; i < this->sizey; i++)
+      {
+	for(unsigned int j = 0; j < this->sizex; j++)
+	  {
+	    this->array[i][j] *= other[i][j];
+	  }
+      }
+  }
+
+  bool hasSameDimensions(Matrix &other)
+  {  
+    unsigned int rsizex = other.getSizeX();
+    unsigned int rsizey = other.getSizeY();
+
+    return ( (rsizex == this->sizex) && 
+	     (rsizey == this->sizey) );
+  }
+
+  bool isSquareMatrix()
+  {
+    return (sizex == sizey);
+  }
+  
+  void print()
+  {
+    for(unsigned int i = 0; i < sizey; i++)
+      {
+	for(unsigned int j = 0; j < sizex; j++)
+	  {
+	    printf(" %i ", this->array[i][j] );
+	  }
+	printf("\n");
+      }
+  }
+
+  /* --------
+    ACCESSORS
+     --------
+  */
+
+  unsigned int getSizeX() { return this->sizex; };
+  unsigned int getSizeY() { return this->sizey; };
+
+private:
+
+  T * data;
+  T ** array;
+
+  unsigned  int sizex, sizey;
+};
+
+#endif
---
+++ tests/cpp/matrix_cstyle.cpp
@@ -0,0 +1,69 @@
+/*    
+    Basic tests for 2D matrix - not very useful yet
+
+    Copyright (C) 2013  H. F. Noelscher
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <assert.h>
+
+#include "../../basics/matrix/matrix_cstyle.hpp"
+
+int main(int argc, char **argv)
+{
+  int sizex = 16;
+  int sizey = 24;
+
+  Matrix<int> mat(sizex, sizey);
+
+  for(int i = 0; i < sizey; i++)
+    {
+      for(int j = 0; j < sizex; j++)
+	{
+	  mat[i][j] = i * j;
+	}
+    }
+
+  // mat.print();
+
+  // integrity?
+  for(int i = 0; i < sizey; i++)
+    {
+      for(int j = 0; j < sizex; j++)
+	{
+	  assert( mat[i][j] == i * j );
+	}
+    }
+
+  // check square matrix detection
+  assert(! mat.isSquareMatrix());
+
+  // check multiplication
+  
+  Matrix<int> first(16,16);
+  Matrix<int> second(16,16);
+
+  first[2][2] = 2;
+  second[2][2] = 3;
+
+  // returning multiplication not yet implemented
+  // Matrix<int> multiplied = first * second;
+  // assert(multiplied[2][2] == 6);
+  
+  first.multInPlace(second);
+  assert( first[2][2] == 6 ); // TBD: extend!
+
+  return 0;
+}
